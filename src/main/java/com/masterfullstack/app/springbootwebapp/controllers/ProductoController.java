8e95a64f720e6caa18fb40b089fa79c1fcff7d55
Javier araya <74607683+cromos92@users.noreply.github.com>
2022-12-26 19:43:44 -0300

    update

--- src/main/java/com/masterfullstack/app/springbootwebapp/controllers/ProductoController.java
+++ src/main/java/com/masterfullstack/app/springbootwebapp/controllers/ProductoController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RequestMapping("/products")
 public class ProductoController {
 
